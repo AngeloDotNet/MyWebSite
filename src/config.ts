3e9b9ccbf477c192b37c91f76ec04fca19a64c95
Angelo Pirola <angelo9871@gmail.com>
2024-01-21 16:49:00 +0100

    versione 1

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Angelo's Personal WebSite";
+export const SITE_URL = "";
